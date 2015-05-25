d9443a72961d5b048e28b601b3374794d21c2ba8
SilenceWang <silencelikesky@sina.com>
2015-05-25 13:51:56 +0800

    add a control script without contents

---
+++ bin/control.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# This control script used for controling spider work/shundown/restart
+# Authorise by Silence. 2015-05-25
+
+usage(){
+	echo "usage: bash control.sh [start|stop|restart]"
+}
+
+usage
